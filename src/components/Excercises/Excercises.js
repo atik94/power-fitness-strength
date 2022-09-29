12528b421bb700cd8146dc7cfc94582f189da083
atik94 <atikurrahmanatik94@gmail.com>
2022-09-29 20:26:03 +0600

    Json value show on component

--- src/components/Excercises/Excercises.js
+++ src/components/Excercises/Excercises.js
@@ -2,13 +2,15 @@ import React from "react";
 import "./Excercises.css";
 const Excercises = ({ excercise, handleAddToCart }) => {
   //const {excercise, handleAddToCart} =props
-  const { name, img, seller, price, ratings } = excercise;
+  const { name, img, details, age, time } = excercise;
   return (
     <div className="excercise">
       <img src={img} alt="" />
       <div className="excercise-info">
         <p className="excercise-name">{name}</p>
-        <p> price :{price}</p>
+        <p>{details ? details.slice(0, 100) : ""}</p>
+        <p>For age: {age}</p>
+        <p>Time required: {time}s</p>
       </div>
       <button onClick={() => handleAddToCart(excercise)} className="btn-cart">
         <p>Add to cart</p>
