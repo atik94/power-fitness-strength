7b466398eb1c2269ee7e816983dad6de27c9b962
atik94 <atikurrahmanatik94@gmail.com>
2022-09-29 21:46:18 +0600

    add personal information

--- src/components/Carts/Carts.js
+++ src/components/Carts/Carts.js
@@ -1,35 +1,46 @@
-import React, {useState } from 'react';
-import './Carts.css'
-const Carts = ({cart}) => {
-    //console.log(cart);
-    let total = 0;
-    for(const product of cart){
-        total = total + product.time;
-    }
-    const [breaks, setBreak] = useState(0);
-    const timeChange=(e)=>{
+import React, { useState } from "react";
+import "./Carts.css";
+const Carts = ({ cart }) => {
+  //console.log(cart);
+  let total = 0;
+  for (const product of cart) {
+    total = total + product.time;
+  }
+  const [breaks, setBreak] = useState(0);
+  const timeChange = (e) => {
+    let bbb = e.target.value;
+    //console.log(bbb);
+    //setBreak(bbb);
 
-        let bbb =e.target.value;
-        //console.log(bbb);
-        //setBreak(bbb);
-
-         localStorage.setItem("item",bbb);
-         const getItems = localStorage.getItem("item");
-         setBreak(getItems);
-    }
-    return (
-        <div className='cart'>
-            <h2>order summary</h2>
-            <p>Selected items : {cart.length}</p>
-            <p>price : {total}s</p>
-            <div>
-                <input onClick={timeChange} type="text" value={10} />
-                <input onClick={timeChange} type="text" value={20} />
-                <p>{breaks}</p>
-                
-            </div>
+    localStorage.setItem("item", bbb);
+    const getItems = localStorage.getItem("item");
+    setBreak(getItems);
+  };
+  return (
+    <div className="cart">
+      <p>price : {total}s</p>
+      <div>
+        <input onClick={timeChange} type="text" value={10} />
+        <input onClick={timeChange} type="text" value={20} />
+        <p>{breaks}</p>
+      </div>
+      <div className="profile">
+        <h2>Atikur Rahman</h2>
+        <p>Dhaka, Bangladesh</p>
+      </div>
+      <div className="myself">
+        <div>
+          <h2>75kg Weight</h2>
         </div>
-    );
+        <div>
+          <h2>4.6 height</h2>
+        </div>
+        <div>
+          <h2>25yrs Age</h2>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Carts;
